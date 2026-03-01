36f245806c9343fb1d13546e719b3d04a9a3b8e7
Thomas <thomasweaverson@gmail.com>
2026-03-01 09:42:28 +0300

    module7-task1 Выполнит домашнее заданее. Рендеринг миниатюр на главной

---
+++ js/gallery.js
@@ -0,0 +1,23 @@
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+const createPicture = ({ comments, description, likes, url }) => {
+  const picture = pictureTemplate.cloneNode(true);
+  picture.querySelector('.picture__img').src = url;
+  picture.querySelector('.picture__img').alt = description;
+  picture.querySelector('.picture__comments').textContent = comments.length;
+  picture.querySelector('.picture__likes').textContent = likes;
+  return picture;
+};
+
+const renderGallery = (photos) => {
+  const fragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    const picture = createPicture(photo);
+    fragment.appendChild(picture);
+  });
+  container.appendChild(fragment);
+};
+
+export { renderGallery };
